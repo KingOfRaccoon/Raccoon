f86d5bd11b18e042b0141ef991cf4570497faebb
Alyosha228 <ivan.gorin123456@gmail.com>
2019-11-20 16:20:09 +0700

    aaaaaaaaaaaaaaaaaaaa

--- app/src/main/java/com/example/raccoon/MainActivity.kt
+++ app/src/main/java/com/example/raccoon/MainActivity.kt
@@ -17,8 +17,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        one.setNMR(1300f, 1)
-        two.setNMR(1300f, 0)
+        one.counterRating = NewRankOther()
+        one.set1(1300f, 1)
+
+        two.set1(1300f, 0)
         f.setText(one.df())
         d.setText(two.df())
     }
@@ -28,27 +30,58 @@ class MainActivity : AppCompatActivity() {
 
 class Player : RatingManager() {
     override var MyRating: Double = 2000.0
-
     fun df() = MyRating.toString()
 }
 
 
 open class RatingManager {
    open var MyRating:Double = 0.0
-    var K:Int = 0
-    var WaitingResult:Double = 0.0
+    var counterRating : NewRank = NewRankElo()
 
-    fun setNMR(ratingEnemy : Float, resulf : Int){
-
-        WaitingResult = 1/(1 +  pow( 10.0 ,(ratingEnemy - MyRating)/400))
-        AccountK()
-        MyRating = MyRating + K *(resulf - WaitingResult)
+    fun set1 ( ratingEnemy : Float, resulf : Int){
+        MyRating = counterRating.setNMR(MyRating, ratingEnemy, resulf)
     }
 
-    fun AccountK() {
+}
+
+
+
+abstract class NewRank{
+    var K:Int = 0
+    abstract fun setNMR( MyRating :Double,ratingEnemy : Float, resulf : Int):Double
+}
+
+
+
+class NewRankElo:NewRank(){
+    override fun setNMR( MyRating :Double,ratingEnemy : Float, resulf : Int):Double{
+        var WaitingResult:Double = 0.0
+        WaitingResult = 1/(1 +  pow( 10.0 ,(ratingEnemy - MyRating)/400))
+        if (WaitingResult > 0.9)
+            WaitingResult = 0.9
+        AccountK(MyRating)
+        return MyRating + K *(resulf - WaitingResult)
+    }
+
+    fun AccountK(MyRating:Double) {
         if (MyRating > 2400f)
             K = 10
         else K = 40
+    }
+}
+class NewRankOther:NewRank(){
+    override fun setNMR( MyRating :Double,ratingEnemy : Float, resulf : Int):Double{
+        var WaitingResult:Double = 0.0
+        WaitingResult = 1/(1 +  pow( 1.0 ,(ratingEnemy - MyRating)/400))
+        if (WaitingResult > 0.9)
+            WaitingResult = 0.9
+        AccountK(MyRating)
+        return MyRating + K *(resulf - WaitingResult)
+    }
 
+    fun AccountK(MyRating:Double) {
+        if (MyRating > 2400f)
+            K = 10
+        else K = 40
     }
 }
