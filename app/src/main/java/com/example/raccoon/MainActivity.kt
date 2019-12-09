2be252b07b3e09d7a0fbad89f4e35eefb22caa23
mariklolik <marshelo44@gmail.com>
2019-12-09 20:11:34 +0700

    aaaaaaaaaaaaaaaaaaa

--- app/src/main/java/com/example/raccoon/MainActivity.kt
+++ app/src/main/java/com/example/raccoon/MainActivity.kt
@@ -9,7 +9,33 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.Math.*
 
 class MainActivity : AppCompatActivity() {
-
+    fun fhgkk():Double {
+        var j: Int = 1
+        var Sum: Double = 0.0
+        var m: Int = 0 //Количество игр
+        var t: Int = 0 //Количество несыгранных периодов
+        var q: Double = log(10.0) / 400
+        var PI: Double = 3.14159
+        var RDold:Double=200.0
+        var RD = min(sqrt(RDold * RDold + 50 * 50 * t).toInt(), 350)
+        var ratingEnemy = 1400
+        var r =1500
+        var resulf =1
+        var RD1 = 30
+        var g: Double  = 1 / sqrt(1 + (3 * q * q) * (RD1 * RD1) / pow(PI, 2.0))
+        Log.d("Test", g.toString())
+        var E: Double = 1 / (1 + pow(10.0, (-g * (r - ratingEnemy)) / 400))
+        Log.d("Test", E.toString())
+        var sum2 = g * (resulf - E)
+        Log.d("Test", sum2.toString())
+        var Sum2 = g * g * E * (1 - E)
+        Log.d("Test", Sum2.toString())
+        var d: Double  = sqrt(1/(q * q * Sum2))
+        Log.d("Test", d.toString())
+        var rN: Double = r + (q / ((1 / RD * RD) + (1 / d * d)) * sum2)
+        Log.d("Test", rN.toString())
+        return rN
+    }
 
     var one = Player()
     var two = Player()
@@ -18,11 +44,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         one.counterRating = NewRankGliko()
-        one.set1(200.0, 1400f, 1500)
-        two.counterRating = NewRankElo()
-        two.set1(200.0, 1400f, 1500)
-        f.setText(one.df())
-        d.setText(two.df())
+        one.set1(200.0, 1400f, 1500, 0)
+        two.counterRating = NewRankGliko()
+        two.set1(200.0, 1400f, 1500, 1)
+f.setText(fhgkk().toString())
     }
 
 }
@@ -38,8 +63,8 @@ open class RatingManager {
    open var MyRating:Double = 0.0
     var counterRating : NewRank = NewRankElo()
 
-    fun set1 ( RDold :Double,ratingEnemy : Float, r : Int){
-        MyRating = counterRating.setNMR(MyRating, ratingEnemy, r)
+    fun set1 ( RDold :Double,ratingEnemy : Float, r : Int, resulf: Int){
+        MyRating = counterRating.setNMR(MyRating, ratingEnemy, r, resulf)
     }
 
 }
@@ -48,14 +73,15 @@ open class RatingManager {
 
 abstract class NewRank{
     var K:Int = 0
-    abstract fun setNMR( MyRating :Double,ratingEnemy : Float, resulf : Int):Double
+    abstract fun setNMR( MyRating :Double,ratingEnemy : Float,r: Int, resulf : Int):Double
+
 
 }
 
 
 
 class NewRankElo:NewRank(){
-    override fun setNMR( MyRating :Double,ratingEnemy : Float, resulf : Int):Double{
+    override fun setNMR( MyRating :Double,ratingEnemy : Float,r: Int, resulf : Int):Double{
         var WaitingResult:Double = 0.0
         WaitingResult = 1/(1 +  pow( 10.0 ,(ratingEnemy - MyRating)/400))
         if (WaitingResult > 0.92)
@@ -73,7 +99,7 @@ class NewRankElo:NewRank(){
     }
 }
 class NewRankOther:NewRank(){
-    override fun setNMR( MyRating :Double,ratingEnemy : Float, resulf : Int):Double{
+    override fun setNMR( MyRating :Double,ratingEnemy : Float,r: Int, resulf : Int):Double{
         var WaitingResult:Double = 0.0
         WaitingResult = 1/(1 +  pow( 1.0 ,(ratingEnemy - MyRating)/400))
         if (WaitingResult > 0.9)
@@ -91,7 +117,7 @@ class NewRankOther:NewRank(){
     }
 }
 class NewRankGliko:NewRank(){
-    override fun setNMR( RDold :Double,s:Float):Double {
+    override fun setNMR(RDold: Double, ratingEnemy: Float, r: Int, resulf: Int): Double {
 
         var rN:Double = 0.0
         var j:Int = 1
@@ -101,19 +127,16 @@ class NewRankGliko:NewRank(){
         var q:Double = log(10.0)/400
         var PI: Double = 3.14159
         var d:Double = 0.0
-var r
         var g : Double = 0.0
-
-        var s : Double = 0.0
-
         var E : Double = 0.0
 
         var RD = min(sqrt (RDold * RDold + 50 * 50 * t ).toInt(), 350)
 
-        g =1/ sqrt(1 + (3 * q * q) * (RD * RD)/ pow(PI, 2.0) )
-        var sum2 = g * (s - E)
-        var Sum2 = g * g * E * (s - E)
         E = 1 / (1 + pow(10.0, (-g * (r-ratingEnemy)))/400 )
+        g =1/ sqrt(1 + (3 * q * q) * (RD * RD)/ pow(PI, 2.0) )
+        var sum2 = g * (resulf - E)
+        var Sum2 = g * g * E * (resulf - E)
+
         d  = sqrt(pow( (q * q * Sum2) ,-1.0))
         rN = r + q / ( (1 / RD * RD)+(1 / d * d ) ) * sum2
 
